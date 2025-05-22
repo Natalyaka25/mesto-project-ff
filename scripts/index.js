d8816c2da226f105b4c1cab52430994ba7546e35
Nataliya Borisova <natalyaka25@yandex.ru>
2025-05-22 15:26:45 +0500

    5 спринт - Mesto(Готовая работа для сдачи)

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,29 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const content = document.querySelector('.content');
+const contentList = content.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function createCard(element) {
+    const card = cardTemplate.querySelector('.places__item').cloneNode(true);
+    card.querySelector('.card__image').src = element.link;
+    card.querySelector('.card__image').alt = element.name;
+    card.querySelector('.card__title').textContent = element.name;
+    const deleteButton = card.querySelector('.card__delete-button')
+    deleteButton.addEventListener('click', deleteCard);
+    return card;
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(evt) {
+    const card = evt.target.closest('.places__item');
+    card.remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(function (element) {
+    const card = createCard(element);
+    contentList.append(card);
+})
