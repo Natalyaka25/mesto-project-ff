d7902a6dbdbd8508154c811f9f93a5c519de341c
Nataliya Borisova <natalyaka25@yandex.ru>
2025-05-22 22:18:17 +0500

    5 спринт - Mesto (Готовая работа для сдачи-исправленное после замечаний)

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,37 @@
 // @todo: Темплейт карточки
-const cardTemplate = document.querySelector('#card-template').content;
+const cardTemplate = document.querySelector("#card-template").content;
+const cardTemplatePlacesItemSelector = '.places__item';
+const cardTemplateImageSelector = '.card__image';
+const cardTemplateTitleSelector = '.card__title';
+const cardTemplateDeleteButtonSelector = '.card__delete-button';
+
 
 // @todo: DOM узлы
-const content = document.querySelector('.content');
-const contentList = content.querySelector('.places__list');
+const content = document.querySelector(".content");
+const contentList = content.querySelector(".places__list");
 
 // @todo: Функция создания карточки
-function createCard(element) {
-    const card = cardTemplate.querySelector('.places__item').cloneNode(true);
-    card.querySelector('.card__image').src = element.link;
-    card.querySelector('.card__image').alt = element.name;
-    card.querySelector('.card__title').textContent = element.name;
-    const deleteButton = card.querySelector('.card__delete-button')
-    deleteButton.addEventListener('click', deleteCard);
-    return card;
+function createCard(element, onDeleteCard) {
+  const card = cardTemplate.querySelector(cardTemplatePlacesItemSelector).cloneNode(true);
+  const cardImage = card.querySelector(cardTemplateImageSelector);
+  const cardTitle = card.querySelector(cardTemplateTitleSelector);
+  cardImage.src = element.link;
+  cardImage.alt = element.name;
+  cardTitle.textContent = element.name;
+
+  const deleteButton = card.querySelector(cardTemplateDeleteButtonSelector);
+  deleteButton.addEventListener("click", onDeleteCard);
+  return card;
 }
 
 // @todo: Функция удаления карточки
-function deleteCard(evt) {
-    const card = evt.target.closest('.places__item');
-    card.remove();
+function handleCardDelete(evt) {
+  const card = evt.target.closest(cardTemplatePlacesItemSelector);
+  card.remove();
 }
 
 // @todo: Вывести карточки на страницу
 initialCards.forEach(function (element) {
-    const card = createCard(element);
-    contentList.append(card);
-})
+  const card = createCard(element, handleCardDelete);
+  contentList.append(card);
+});
